c0e4f9e212c54014f9ade3d8817276ae51e46ec4
usuariogenerico123 <negritolinuxero@gmail.com>
2026-08-02 03:48:33 +0000

    eeqw2

--- pelis/pelisBack/loadEnvVariables.sh
+++ pelis/pelisBack/loadEnvVariables.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#postgresql://neondb_owner:npg_laL0SXp4dJnv@ep-lively-salad-ayg69gm7-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require
 
-JWT_SECRET="mikis"
-URL_DB="host=ep-lively-salad-ayg69gm7-pooler.c-5.us-east-2.aws.neon.tech user=neondb_owner password=npg_laL0SXp4dJnv dbname=neondb"
+JWT_SECRET=""
+URL_DB=""
 
 export JWT_SECRET=$JWT_SECRET
 export URL_DB=$URL_DB
